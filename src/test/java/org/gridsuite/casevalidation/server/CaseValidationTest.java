dd4d6b91aa45bc4b4adb8b8a3831b0bd68a96540
HOMER Etienne <etiennehomer@gmail.com>
2020-07-20 14:07:38 +0200

    Implement unit tests
    Signed-off-by: HOMER Etienne <etiennehomer@gmail.com>

--- src/test/java/org/gridsuite/casevalidation/server/CaseValidationTest.java
+++ src/test/java/org/gridsuite/casevalidation/server/CaseValidationTest.java
@@ -6,7 +6,15 @@
  */
 package org.gridsuite.casevalidation.server;
 
+import com.powsybl.commons.PowsyblException;
+import com.powsybl.iidm.network.Network;
+import com.powsybl.iidm.network.test.EurostagTutorialExample1Factory;
+import com.powsybl.loadflow.LoadFlowParameters;
+import com.powsybl.loadflow.json.JsonLoadFlowParameters;
 import com.powsybl.network.store.client.NetworkStoreService;
+import com.powsybl.network.store.client.PreloadingStrategy;
+import com.powsybl.openloadflow.OpenLoadFlowParameters;
+import com.powsybl.openloadflow.network.MostMeshedSlackBusSelector;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,11 +22,20 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import java.io.ByteArrayOutputStream;
+import java.util.UUID;
 
 import static org.junit.Assert.assertTrue;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
  * @author Etienne Homer <etienne.homer at rte-france.com>
@@ -40,6 +57,45 @@ public class CaseValidationTest {
 
     @Test
     public void test() throws Exception {
-        assertTrue(true);
+        UUID testNetworkId = UUID.fromString("7928181c-7977-4592-ba19-88027e4254e4");
+        UUID notFoundNetworkId = UUID.fromString("aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa");
+
+        given(networkStoreService.getNetwork(testNetworkId, PreloadingStrategy.COLLECTION)).willReturn(createNetwork());
+        given(networkStoreService.getNetwork(notFoundNetworkId, PreloadingStrategy.COLLECTION)).willThrow(new PowsyblException());
+
+        // network not existing
+        mvc.perform(put("/v1/networks/{networkUuid}/check", notFoundNetworkId))
+                .andExpect(status().isNotFound());
+
+        // load flow without parameters (default parameters are used)
+        MvcResult result = mvc.perform(put("/v1/networks/{networkUuid}/check", testNetworkId))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andReturn();
+        assertTrue(result.getResponse().getContentAsString().contains("status\":\"CONVERGED\""));
+
+        // load flow with parameters
+        LoadFlowParameters params = new LoadFlowParameters();
+        params.setNoGeneratorReactiveLimits(true);
+        OpenLoadFlowParameters parametersExt = new OpenLoadFlowParameters()
+                .setSlackBusSelector(new MostMeshedSlackBusSelector())
+                .setDistributedSlack(false);
+        params.addExtension(OpenLoadFlowParameters.class, parametersExt);
+
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        JsonLoadFlowParameters.write(params, stream);
+        String paramsString = new String(stream.toByteArray());
+
+        result = mvc.perform(put("/v1/networks/{networkUuid}/check", testNetworkId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(paramsString))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andReturn();
+        assertTrue(result.getResponse().getContentAsString().contains("status\":\"CONVERGED\""));
+    }
+
+    public Network createNetwork() {
+        return EurostagTutorialExample1Factory.create();
     }
 }
