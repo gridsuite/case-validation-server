65c557eee91664fd51d28b9d961dc1a4bf29e4e7
etiennehomer <etiennehomer@gmail.com>
2020-07-22 14:29:57 +0200

    Fix case validation server (#4)
    * Add powsybl-config-classic depedency + clean licence
    
    Signed-off-by: HOMER Etienne <etiennehomer@gmail.com>
    
    * Clean license
    
    Signed-off-by: HOMER Etienne <etiennehomer@gmail.com>

--- src/main/java/org/gridsuite/casevalidation/server/CaseValidationApi.java
+++ src/main/java/org/gridsuite/casevalidation/server/CaseValidationApi.java
@@ -1,7 +1,4 @@
 /**
-=======
-package org.gridsuite.casevalidation.server; /**
->>>>>>> master
  * Copyright (c) 2020, RTE (http://www.rte-france.com)
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
