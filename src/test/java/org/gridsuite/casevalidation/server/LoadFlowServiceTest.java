082eb072a70123eaa931184390c9a970477e3a05
HOMER Etienne <etiennehomer@gmail.com>
2021-03-29 14:12:20 +0200

    Clean test
    Signed-off-by: HOMER Etienne <etiennehomer@gmail.com>

--- src/test/java/org/gridsuite/casevalidation/server/LoadFlowServiceTest.java
+++ src/test/java/org/gridsuite/casevalidation/server/LoadFlowServiceTest.java
@@ -18,7 +18,6 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
