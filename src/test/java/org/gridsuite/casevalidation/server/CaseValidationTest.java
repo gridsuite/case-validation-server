e84a17a63cf05b108dc372ec9bca12ba16e61c5d
Abdelsalem <abdelsalem.hedhili@rte-france.com>
2025-10-16 11:12:20 +0200

    springboot 3.4.9
    Signed-off-by: Abdelsalem <abdelsalem.hedhili@rte-france.com>

--- src/test/java/org/gridsuite/casevalidation/server/CaseValidationTest.java
+++ src/test/java/org/gridsuite/casevalidation/server/CaseValidationTest.java
@@ -15,9 +15,9 @@ import org.junit.jupiter.api.Test;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
@@ -43,7 +43,7 @@ class CaseValidationTest {
     @Autowired
     private MockMvc mvc;
 
-    @MockBean
+    @MockitoBean
     private LoadFlowService loadFlowService;
 
     @BeforeEach
